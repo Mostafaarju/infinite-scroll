81e5d70b5f633e714a56cb4ba43ece4df0d3f4aa
Raju Talukder <63764709+Mostafaarju@users.noreply.github.com>
2021-02-20 17:23:29 +0100

    first commit

--- script.js
+++ script.js
@@ -79,29 +79,3 @@ window.addEventListener('scroll', () => {
 
 // On Load
 getPhotos();
-
-const videoElement = document.getElementById('video');
-const button = document.getElementById('button');
-
-async function selectMediaStream() {
-    try {
-        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-        videoElement.srcObject = mediaStream;
-        videoElement.onloadedmetadata = () => {
-            videoElement.play();
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-button.addEventListener('click', async () => {
-    // Disable button
-    button.disabled = true;
-    // Start Picture in Picture
-    await videoElement.requestPictureInPicture();
-    // Reset Button
-    button.disabled = false;
-});
-
-selectMediaStream();
